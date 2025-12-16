e0cf53422ba93ef8e78c4a572f10257fc7b677e5
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2025-12-16 14:23:12 +0100

    feat:start to add more functions to be able to separate by house

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 const requestCharactersURL = `https://thronesapi.com/api/v2/Characters`;
-const requestContinentsURL = `https://thronesapi.com/api/v2/Characters`;
+const requestContinentsURL = `https://thronesapi.com/api/v2/Continents`;
 
 function createCharactersCard({ firstName, lastName, imageUrl, title, family}) {
-
     return `
         <div class="card">
             <img src="${imageUrl}" alt="Pinture of the character">
@@ -13,8 +12,18 @@ function createCharactersCard({ firstName, lastName, imageUrl, title, family}) {
     `;
 }
 
-function createContinentsCard({name}) {
+function createStarkCard({ firstName, lastName, imageUrl, title, family}) {
+    return `
+        <div class="card house-stark">
+            <img src="${imageUrl}" alt="Pinture of the character">
+            <h2>${firstName} ${lastName}</h2>
+            <h3>${family}</h3>
+            <h3>${title}</h3>
+        </div>
+    `;
+}
 
+function createContinentsCard({name}) {
     return `
         <div class="card">
             <h2>${name}</h2>
@@ -48,19 +57,6 @@ async function fetchContinentsThroneJson() {
     }
 }
 
-async function displayCharactersThrone() {
-    const charactersSection = document.getElementById('throneCharactersSection');
-    const charactersData = await fetchCharactersThroneJson();
-    if (charactersData ) {
-        const charactersCards = charactersData.map(createCharactersCard).join('');
-        charactersSection.innerHTML = charactersCards;
-    } else {
-        charactersSection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
-    }
-}
-
-displayCharactersThrone();
-
 async function displayContinentsThrone() {
     const continentsSection = document.getElementById('throneContinentsSection');
     const continentsData = await fetchContinentsThroneJson();
@@ -73,3 +69,32 @@ async function displayContinentsThrone() {
 }
 
 displayContinentsThrone();
+
+async function displayCharactersThrone() {
+    const charactersSection = document.getElementById('throneCharactersSection');
+    const charactersData = await fetchCharactersThroneJson();
+    if (charactersData) {
+        const charactersCards = charactersData.map(createCharactersCard).join('');
+        charactersSection.innerHTML = charactersCards;
+    } else {
+        charactersSection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
+    }
+}
+
+displayCharactersThrone();
+
+
+
+async function displayHouseStarkThrone() {
+    const starkSection = document.getElementById('houseStarkSection');
+    const houseStarkData = await fetchCharactersThroneJson();
+    if (houseStarkData ) {
+        const starkCharacters = houseStarkData.filter(character => character.family === 'House Stark');
+        const charactersStarkCards = starkCharacters.map(createStarkCard).join('');
+        starkSection.innerHTML = charactersStarkCards;
+    } else {
+        starkSection.innerHTML = '<p>Los Starks han escapado de nosotros.</p>';
+    }
+}
+
+displayHouseStarkThrone();
