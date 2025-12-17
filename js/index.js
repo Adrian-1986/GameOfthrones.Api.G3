dc8851271f029d5a42a0aa5ff3307f814b48369e
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2025-12-17 14:27:55 +0100

    trying to solve a problem from merging the files

--- js/index.js
+++ js/index.js
@@ -30,116 +30,6 @@ function createStarkCard({ firstName, lastName, imageUrl, title, family}) {
     `;
 }
 
-function createContinentsCard({name}) {
-    return `
-        <div class="card">
-            <h2>${name}</h2>
-        </div>
-    `;
-}
-
-async function fetchCharactersThroneJson() {
-    try {
-        const response = await fetch(requestCharactersURL);
-        if (!response.ok) {
-            throw new Error(`Error de la solucitud: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Error al obtener el archivo JSON:', error);
-        return null;
-    }
-}
-
-async function fetchContinentsThroneJson() {
-    try {
-        const response = await fetch(requestContinentsURL);
-        if (!response.ok) {
-            throw new Error(`Error de la solucitud: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Error al obtener el archivo JSON:', error);
-        return null;
-    }
-}
-
-async function displayContinentsThrone() {
-    const continentsSection = document.getElementById('throneContinentsSection');
-    const continentsData = await fetchContinentsThroneJson();
-    if (continentsData ) {
-        const continentsCards = continentsData.map(createContinentsCard).join('');
-        continentsSection.innerHTML = continentsCards;
-    } else {
-        continentsSection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
-    }
-}
-
-displayContinentsThrone();
-
-async function displayCharactersThrone() {
-    const charactersSection = document.getElementById('throneCharactersSection');
-    const charactersData = await fetchCharactersThroneJson();
-    if (charactersData) {
-        const charactersCards = charactersData.map(createCharactersCard).join('');
-        charactersSection.innerHTML = charactersCards;
-    } else {
-        charactersSection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
-    }
-}
-
-displayCharactersThrone();
-
-
-
-async function displayHouseStarkThrone() {
-    const starkSection = document.getElementById('houseStarkSection');
-    const houseStarkData = await fetchCharactersThroneJson();
-    if (houseStarkData ) {
-        const starkCharacters = houseStarkData.filter(character => character.family === 'House Stark');
-        const charactersStarkCards = starkCharacters.map(createStarkCard).join('');
-        starkSection.innerHTML = charactersStarkCards;
-    } else {
-        starkSection.innerHTML = '<p>Los Starks han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseStarkThrone();
-
-const requestCharactersURL = `https://thronesapi.com/api/v2/Characters`;
-const requestContinentsURL = `https://thronesapi.com/api/v2/Continents`;
-
-
-
-function createCharactersCard({ firstName, lastName, imageUrl, title, family}) {
-    return `
-        <div class="card">
-            <img src="${imageUrl}" alt="Pinture of the character">
-            <h2>${firstName} ${lastName}</h2>
-            <h3>${family}</h3>
-            <h3>${title}</h3>
-        </div>
-    `;
-}
-
-function createStarkCard({ firstName, lastName, imageUrl, title, family}) {
-    return `
-        <div class="card house-stark">
-            <img src="${imageUrl}" alt="Pinture of the character">
-            <div class="stark-info">
-                <div class="stark-text">
-                    <h2>${firstName} ${lastName}</h2>
-                    <h3>${family}</h3>
-                    <h3>${title}</h3>
-                </div>
-                <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
-                </div>
-            </div>
-        </div>
-    `;
-}
-
 function createLannisterCard({ firstName, lastName, imageUrl, title, family}) {
     return `
         <div class="card house-lannister">
@@ -165,8 +55,6 @@ function createContinentsCard({name}) {
         </div>
     `;
 }
-
-
 
 async function fetchCharactersThroneJson() {
     try {
