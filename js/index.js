f6ea66997c9e2aad0ef5a84f177b793381278cad
Adrian-1986 <adryhonor9@gmail.com>
2025-12-18 08:52:55 +0100

    re-raised

--- js/index.js
+++ js/index.js
@@ -11,6 +11,17 @@ function createCharactersCard({ firstName, lastName, imageUrl, title, family}) {
         </div>
     `;
 }
+function createratBaheonCard({ firstName, lastName, imageUrl, title, family}) {
+
+    return `
+        <div class="card house-baratheon">
+            <img src="${imageUrl}" alt="Pinture of the character">
+            <h2>${firstName} ${lastName}</h2>
+            <h3>${family}</h3>
+            <h3>${title}</h3>
+        </div>
+    `;
+}
 
 async function fetchThroneJson() {
     try {
@@ -37,3 +48,30 @@ async function displayThrone() {
 }
 
 displayThrone();
+
+async function displayBaheon() {
+    const baheonSection = document.getElementById('BaheonSection');
+    const BaheonData = await fetchThroneJson();
+    if (BaheonData ) {
+        const baratheonCharacters = BaheonData.filter(character => character.family === 'House Baratheon');
+        const BaheonCards = baratheonCharacters.map(createratBaheonCard).join('');
+        baheonSection.innerHTML = BaheonCards;
+    } else {
+        baheonSection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
+    }
+}
+
+displayBaheon();
+
+async function displayGreyjoy() {
+    const  GreyjoySection = document.getElementById('GreyjoySection');
+    const  GreyjoyData = await fetchGreyjoyJson();
+    if ( GreyjoyData ) {
+        const  greyjoyCharacters =  GreyjoyData.filter(character => character.family === 'House Greyjoy');
+        const  greyjoyCards =  greyjoyCharacters.map(createGreyjoyCard).join('');
+        GreyjoySection.innerHTML =  greyjoyCards;
+    } else {
+        GreyjoySection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
+    }
+}
+displayGreyjoy();
