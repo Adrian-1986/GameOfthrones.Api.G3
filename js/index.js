d93e4ab6b838c7b550ee98a0aa75012e3efcb480
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2025-12-18 13:44:17 +0100

    fix last changes on styles and js

--- js/index.js
+++ js/index.js
@@ -103,48 +103,6 @@ async function displayContinentsThrone() {
 
 displayContinentsThrone();
 
-async function displayHouseStarkThrone() {
-    const starkSection = document.getElementById('houseStarkSection');
-    const houseStarkData = await fetchCharactersThroneJson();
-    if (houseStarkData ) {
-        const starkCharacters = houseStarkData.filter(character => character.family === 'House Stark' || character.family === 'Stark');
-        const charactersStarkCards = starkCharacters.map(createStarkCard).join('');
-        starkSection.innerHTML = charactersStarkCards;
-    } else {
-        starkSection.innerHTML = '<p>Los Starks han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseStarkThrone();
-
-async function displayHouseLannisterThrone() {
-    const lannisterSection = document.getElementById('houseLannisterSection');
-    const houseLannisterData = await fetchCharactersThroneJson();
-    if (houseLannisterData ) {
-        const lannisterCharacters = houseLannisterData.filter(character => character.family === 'House Lannister' || character.family === 'House Lanister' || character.family === 'Lanister' || character.family === 'Lannister');
-        const charactersLannisterCards = lannisterCharacters.map(createLannisterCard).join('');
-        lannisterSection.innerHTML = charactersLannisterCards;
-    } else {
-        lannisterSection.innerHTML = '<p>Los Starks han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseLannisterThrone();
-
-async function displayHouseTargaryenThrone() {
-    const targaryenSection = document.getElementById('houseTargaryenSection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        const targaryenCharacters = characterData.filter(character => character.family === 'House Targaryen'|| character.family === 'Targaryan');
-        const charactersTargaryenCards = targaryenCharacters.map(createTargaryenCard).join('');
-        targaryenSection.innerHTML = charactersTargaryenCards;
-    } else {
-        targaryenSection.innerHTML = '<p>Los Starks han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseTargaryenThrone();
-
 function createBaratheonCard({ firstName, lastName, imageUrl, title, family}) {
     return `
         <div class="card house-baratheon">
@@ -162,21 +120,6 @@ function createBaratheonCard({ firstName, lastName, imageUrl, title, family}) {
         </div>
     `;
 }
-
-async function displayHouseBaratheonThrone() {
-    const baratheonSection = document.getElementById('houseTargaryenSection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        const baratheonCharacters = characterData.filter(character => character.family === 'House Baratheon'|| character.family === 'Baratheon');
-        const charactersBaratheonCards = baratheonCharacters.map(createBaratheonCard).join('');
-        baratheonSection.innerHTML = charactersBaratheonCards;
-    } else {
-        baratheonSection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseBaratheonThrone();
-
 
 function createTyrellCard({ firstName, lastName, imageUrl, title, family}) {
     return `
@@ -196,20 +139,6 @@ function createTyrellCard({ firstName, lastName, imageUrl, title, family}) {
     `;
 }
 
-async function displayHouseTyrellThrone() {
-    const tyrellSection = document.getElementById('houseTyrellSection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        const tyrellCharacters = characterData.filter(character => character.family === 'House Tyrell'|| character.family === 'Tyrell');
-        const charactersTyrellCards = tyrellCharacters.map(createBaratheonCard).join('');
-        tyrellSection.innerHTML = charactersTyrellCards;
-    } else {
-        tyrellSection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseTyrellThrone();
-
 function createMormontCard({ firstName, lastName, imageUrl, title, family}) {
     return `
         <div class="card house-mormont">
@@ -221,26 +150,12 @@ function createMormontCard({ firstName, lastName, imageUrl, title, family}) {
                     <h3>${title}</h3>
                 </div>
                 <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
+                    <img class="stark-sigil" src="./assets/img/houseMormontSigil.png" alt="House Stark Sigil" class="stark-sigil">
                 </div>
             </div>
         </div>
     `;
 }
-
-async function displayHouseMormontThrone() {
-    const mormontSection = document.getElementById('houseMormontSection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        const mormontCharacters = characterData.filter(character => character.family === 'House Mormont'|| character.family === 'Mormont');
-        const charactersMormontCards = mormontCharacters.map(createBaratheonCard).join('');
-        mormontSection.innerHTML = charactersMormontCards;
-    } else {
-        mormontSection.innerHTML = '<p>Los Mormont han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseMormontThrone();
 
 function createBoltonCard({ firstName, lastName, imageUrl, title, family}) {
     return `
@@ -260,20 +175,6 @@ function createBoltonCard({ firstName, lastName, imageUrl, title, family}) {
     `;
 }
 
-async function displayHouseBoltonThrone() {
-    const boltonSection = document.getElementById('houseBoltonSection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        const boltonCharacters = characterData.filter(character => character.family === 'House Bolton'|| character.family === 'Bolton');
-        const charactersBoltonCards = boltonCharacters.map(createBaratheonCard).join('');
-        boltonSection.innerHTML = charactersBoltonCards;
-    } else {
-        boltonSection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseBoltonThrone();
-
 function createGreyjoyCard({ firstName, lastName, imageUrl, title, family}) {
     return `
         <div class="card house-greyjoy">
@@ -292,19 +193,20 @@ function createGreyjoyCard({ firstName, lastName, imageUrl, title, family}) {
     `;
 }
 
-async function displayHouseGreyjoyThrone() {
-    const greyjoySection = document.getElementById('houseGreyjoySection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        constgGreyjoyCharacters = characterData.filter(character => character.family === 'House Greyjoy'|| character.family === 'Greyjoy');
-        const charactersGreyjoyCards = greyjoyCharacters.map(createBaratheonCard).join('');
-        greyjoySection.innerHTML = charactersGreyjoyCards;
-    } else {
-        greyjoySection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
-    }
+function createOthersCard({ firstName, lastName, imageUrl, title, family}) {
+    return `
+        <div class="card house-greyjoy">
+            <img src="${imageUrl}" alt="Pinture of the character">
+            <div class="stark-info">
+                <div class="stark-text">
+                    <h2>${firstName} ${lastName}</h2>
+                    <h3>${family}</h3>
+                    <h3>${title}</h3>
+                </div>
+            </div>
+        </div>
+    `;
 }
-
-displayHouseBoltonThrone();
 
 async function displayAllHousesThrone() {
     const AllHousesSection = document.getElementById('allHousesSection');
@@ -321,14 +223,14 @@ async function displayAllHousesThrone() {
         const usedFamilies = ['House Greyjoy', 'Greyjoy','House Targaryen','House Targaryan', 'Targaryan','House Lannister', 'Lannister', 'House Lanister', 'Lanister', 'House Stark', 'Stark', 'House Bolton', 'Bolton', 'House Mormont', 'Mormont', 'House Tyrell', 'Tyrell', 'House Baratheon', 'Baratheon'];
         const othersHouses = characterData.filter(character => !usedFamilies.includes(character.family));
         const baratheonCards = baratheonCharacters.map(createBaratheonCard).join('');
-        const greyjoyCards = greyjoyCharacters.map(createBaratheonCard).join('');
-        const tyrellCards = tyrellCharacters.map(createBaratheonCard).join('');
-        const mormontCards = mormontCharacters.map(createBaratheonCard).join('');
-        const boltonCards = boltonCharacters.map(createBaratheonCard).join('');
+        const greyjoyCards = greyjoyCharacters.map(createGreyjoyCard).join('');
+        const tyrellCards = tyrellCharacters.map(createTyrellCard).join('');
+        const mormontCards = mormontCharacters.map(createMormontCard).join('');
+        const boltonCards = boltonCharacters.map(createBoltonCard).join('');
         const lannisterCards = lannisterCharacters.map(createLannisterCard).join('');
         const starkCards = starkCharacters.map(createStarkCard).join('');
-        const othersCards = othersHouses.map(createStarkCard).join('');
-        const targaryenCards = targaryenCharacters.map(createLannisterCard).join('');
+        const othersCards = othersHouses.map(createOthersCard).join('');
+        const targaryenCards = targaryenCharacters.map(createTargaryenCard).join('');
         const allHousesCards = lannisterCards + starkCards + boltonCards + mormontCards + tyrellCards + baratheonCards + othersCards + greyjoyCards + targaryenCards;
         AllHousesSection.innerHTML = allHousesCards;
     } else {
