d79be2748b1fd9d064d3d82d5b3ce7a078375b39
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2025-12-18 13:27:29 +0100

    fix images front cards

--- js/index.js
+++ js/index.js
@@ -19,6 +19,24 @@ function createStarkCard({ firstName, lastName, imageUrl, title, family}) {
     `;
 }
 
+function createTargaryenCard({ firstName, lastName, imageUrl, title, family}) {
+    return `
+        <div class="card house-targaryen">
+            <img src="${imageUrl}" alt="Pinture of the character">
+            <div class="stark-info">
+                <div class="stark-text">
+                    <h2>${firstName} ${lastName}</h2>
+                    <h3>${family}</h3>
+                    <h3>${title}</h3>
+                </div>
+                <div class="stark-sigil-container">
+                    <img class="stark-sigil" src="./assets/img/targaryenSigil.png" alt="House Stark Sigil" class="stark-sigil">
+                </div>
+            </div>
+        </div>
+    `;
+}
+
 function createLannisterCard({ firstName, lastName, imageUrl, title, family}) {
     return `
         <div class="card house-lannister">
@@ -30,7 +48,7 @@ function createLannisterCard({ firstName, lastName, imageUrl, title, family}) {
                     <h3>${title}</h3>
                 </div>
                 <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
+                    <img class="stark-sigil" src="./assets/img/lannisterSigil.png" alt="House Stark Sigil" class="stark-sigil">
                 </div>
             </div>
         </div>
@@ -85,19 +103,6 @@ async function displayContinentsThrone() {
 
 displayContinentsThrone();
 
-async function displayCharactersThrone() {
-    const charactersSection = document.getElementById('throneCharactersSection');
-    const charactersData = await fetchCharactersThroneJson();
-    if (charactersData) {
-        const charactersCards = charactersData.map(createCharactersCard).join('');
-        charactersSection.innerHTML = charactersCards;
-    } else {
-        charactersSection.innerHTML = '<p>No hemos podido entrar en Invernalia.</p>';
-    }
-}
-
-displayCharactersThrone();
-
 async function displayHouseStarkThrone() {
     const starkSection = document.getElementById('houseStarkSection');
     const houseStarkData = await fetchCharactersThroneJson();
@@ -131,7 +136,7 @@ async function displayHouseTargaryenThrone() {
     const characterData = await fetchCharactersThroneJson();
     if (characterData) {
         const targaryenCharacters = characterData.filter(character => character.family === 'House Targaryen'|| character.family === 'Targaryan');
-        const charactersTargaryenCards = targaryenCharacters.map(createLannisterCard).join('');
+        const charactersTargaryenCards = targaryenCharacters.map(createTargaryenCard).join('');
         targaryenSection.innerHTML = charactersTargaryenCards;
     } else {
         targaryenSection.innerHTML = '<p>Los Starks han escapado de nosotros.</p>';
@@ -151,7 +156,7 @@ function createBaratheonCard({ firstName, lastName, imageUrl, title, family}) {
                     <h3>${title}</h3>
                 </div>
                 <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
+                    <img class="stark-sigil" src="./assets/img/baratheonSigil.png" alt="House Stark Sigil" class="stark-sigil">
                 </div>
             </div>
         </div>
@@ -172,39 +177,6 @@ async function displayHouseBaratheonThrone() {
 
 displayHouseBaratheonThrone();
 
-function createGreyjoyCard({ firstName, lastName, imageUrl, title, family}) {
-    return `
-        <div class="card house-greyjoy">
-            <img src="${imageUrl}" alt="Pinture of the character">
-            <div class="stark-info">
-                <div class="stark-text">
-                    <h2>${firstName} ${lastName}</h2>
-                    <h3>${family}</h3>
-                    <h3>${title}</h3>
-                </div>
-                <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
-                </div>
-            </div>
-        </div>
-    `;
-}
-
-async function displayHouseGreyjoyThrone() {
-    const greyjoySection = document.getElementById('houseGreyjoySection');
-    const characterData = await fetchCharactersThroneJson();
-    if (characterData) {
-        const greyjoyCharacters = characterData.filter(character => character.family === 'House Greyjoy'|| character.family === 'Baratheon');
-        const charactersGreyjoyCards = greyjoyCharacters.map(createBaratheonCard).join('');
-        greyjoySection.innerHTML = charactersGreyjoyCards;
-    } else {
-        greyjoySection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
-    }
-}
-
-displayHouseGreyjoyThrone();
-
-
 
 function createTyrellCard({ firstName, lastName, imageUrl, title, family}) {
     return `
@@ -217,7 +189,7 @@ function createTyrellCard({ firstName, lastName, imageUrl, title, family}) {
                     <h3>${title}</h3>
                 </div>
                 <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
+                    <img class="stark-sigil" src="./assets/img/houseTyrellSigil.png" alt="House Stark Sigil" class="stark-sigil">
                 </div>
             </div>
         </div>
@@ -240,7 +212,7 @@ displayHouseTyrellThrone();
 
 function createMormontCard({ firstName, lastName, imageUrl, title, family}) {
     return `
-        <div class="card house-Mormont">
+        <div class="card house-mormont">
             <img src="${imageUrl}" alt="Pinture of the character">
             <div class="stark-info">
                 <div class="stark-text">
@@ -264,7 +236,7 @@ async function displayHouseMormontThrone() {
         const charactersMormontCards = mormontCharacters.map(createBaratheonCard).join('');
         mormontSection.innerHTML = charactersMormontCards;
     } else {
-        mormontSection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
+        mormontSection.innerHTML = '<p>Los Mormont han escapado de nosotros.</p>';
     }
 }
 
@@ -272,7 +244,7 @@ displayHouseMormontThrone();
 
 function createBoltonCard({ firstName, lastName, imageUrl, title, family}) {
     return `
-        <div class="card house-Bolton">
+        <div class="card house-bolton">
             <img src="${imageUrl}" alt="Pinture of the character">
             <div class="stark-info">
                 <div class="stark-text">
@@ -281,7 +253,7 @@ function createBoltonCard({ firstName, lastName, imageUrl, title, family}) {
                     <h3>${title}</h3>
                 </div>
                 <div class="stark-sigil-container">
-                    <img class="stark-sigil" src="./assets/img/houseStark.png" alt="House Stark Sigil" class="stark-sigil">
+                    <img class="stark-sigil" src="./assets/img/houseBoltonSigil.png" alt="House Stark Sigil" class="stark-sigil">
                 </div>
             </div>
         </div>
@@ -302,6 +274,38 @@ async function displayHouseBoltonThrone() {
 
 displayHouseBoltonThrone();
 
+function createGreyjoyCard({ firstName, lastName, imageUrl, title, family}) {
+    return `
+        <div class="card house-greyjoy">
+            <img src="${imageUrl}" alt="Pinture of the character">
+            <div class="stark-info">
+                <div class="stark-text">
+                    <h2>${firstName} ${lastName}</h2>
+                    <h3>${family}</h3>
+                    <h3>${title}</h3>
+                </div>
+                <div class="stark-sigil-container">
+                    <img class="stark-sigil" src="./assets/img/greyjoySigil.png" alt="House Stark Sigil" class="stark-sigil">
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+async function displayHouseGreyjoyThrone() {
+    const greyjoySection = document.getElementById('houseGreyjoySection');
+    const characterData = await fetchCharactersThroneJson();
+    if (characterData) {
+        constgGreyjoyCharacters = characterData.filter(character => character.family === 'House Greyjoy'|| character.family === 'Greyjoy');
+        const charactersGreyjoyCards = greyjoyCharacters.map(createBaratheonCard).join('');
+        greyjoySection.innerHTML = charactersGreyjoyCards;
+    } else {
+        greyjoySection.innerHTML = '<p>Los Baratheon han escapado de nosotros.</p>';
+    }
+}
+
+displayHouseBoltonThrone();
+
 async function displayAllHousesThrone() {
     const AllHousesSection = document.getElementById('allHousesSection');
     const characterData = await fetchCharactersThroneJson();
@@ -312,10 +316,12 @@ async function displayAllHousesThrone() {
         const mormontCharacters = characterData.filter(character => character.family === 'House Mormont'|| character.family === 'Mormont');
         const tyrellCharacters = characterData.filter(character => character.family === 'House Tyrell'|| character.family === 'Tyrell');
         const baratheonCharacters = characterData.filter(character => character.family === 'House Baratheon' || character.family === 'Baratheon');
+        const greyjoyCharacters = characterData.filter(character => character.family === 'House Greyjoy' || character.family === 'Greyjoy');
         const targaryenCharacters = characterData.filter(character => character.family === 'House Targaryen' || character.family === 'Targaryan'|| character.family === 'Targaryan');
-        const usedFamilies = ['House Targaryen','House Targaryan', 'Targaryan','House Lannister', 'Lannister', 'House Lanister', 'Lanister', 'House Stark', 'Stark', 'House Bolton', 'Bolton', 'House Mormont', 'Mormont', 'House Tyrell', 'Tyrell', 'House Baratheon', 'Baratheon'];
+        const usedFamilies = ['House Greyjoy', 'Greyjoy','House Targaryen','House Targaryan', 'Targaryan','House Lannister', 'Lannister', 'House Lanister', 'Lanister', 'House Stark', 'Stark', 'House Bolton', 'Bolton', 'House Mormont', 'Mormont', 'House Tyrell', 'Tyrell', 'House Baratheon', 'Baratheon'];
         const othersHouses = characterData.filter(character => !usedFamilies.includes(character.family));
         const baratheonCards = baratheonCharacters.map(createBaratheonCard).join('');
+        const greyjoyCards = greyjoyCharacters.map(createBaratheonCard).join('');
         const tyrellCards = tyrellCharacters.map(createBaratheonCard).join('');
         const mormontCards = mormontCharacters.map(createBaratheonCard).join('');
         const boltonCards = boltonCharacters.map(createBaratheonCard).join('');
@@ -323,10 +329,10 @@ async function displayAllHousesThrone() {
         const starkCards = starkCharacters.map(createStarkCard).join('');
         const othersCards = othersHouses.map(createStarkCard).join('');
         const targaryenCards = targaryenCharacters.map(createLannisterCard).join('');
-        const allHousesCards = lannisterCards + starkCards + boltonCards + mormontCards + tyrellCards + baratheonCards + othersCards + targaryenCards;
+        const allHousesCards = lannisterCards + starkCards + boltonCards + mormontCards + tyrellCards + baratheonCards + othersCards + greyjoyCards + targaryenCards;
         AllHousesSection.innerHTML = allHousesCards;
     } else {
-        AllHousesSection.innerHTML = '<p>Los Lannister han escapado de nosotros.</p>';
+        AllHousesSection.innerHTML = '<p>No hemos podido encontrarlos.</p>';
     }
 }
 
